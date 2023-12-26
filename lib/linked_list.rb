1b797e5f5f4fe3192d2b79d469651986001d3601
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-26 13:28:24 -0500

    feat: count nodes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,4 +10,16 @@ class LinkedList
   def append(string)
     @head = Node.new(string)
   end
+
+  def count
+    count = 0
+
+    if @head == nil
+      return count
+    else
+      count = 1
+    end
+
+    count
+  end
 end
