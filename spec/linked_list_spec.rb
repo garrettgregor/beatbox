35438092a7e61252a6ed1cd9feb81505a2a2ec21
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-27 08:39:25 -0500

    test: #count_node

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -16,8 +16,20 @@ RSpec.describe LinkedList do
       it "adds a node to the linked list" do
         list.append("doop")
 
+        expect(list.head).to be_a(Node)
         expect(list.head.data).to eq("doop")
         expect(list.head.next_node).to eq(nil)
+      end
+
+      it "can append multiple nodes that are added to the end of the list" do
+        list.append("doop")
+        list.append("deep")
+
+        expect(list.head).to be_a(Node)
+        expect(list.head.data).to eq("doop")
+        expect(list.head.next_node).to be_a(Node)
+        expect(list.head.next_node.data).to eq("deep")
+
       end
     end
 
@@ -26,6 +38,32 @@ RSpec.describe LinkedList do
         list.append("doop")
 
         expect(list.count).to eq(1)
+
+        list.append("deep")
+
+        expect(list.count).to eq(2)
+      end
+    end
+
+    context "#to_string" do
+      it "makes a string out of the linked list data" do
+        expect(list.to_string).to eq("")
+
+        list.append("doop")
+
+        expect(list.to_string).to eq("doop")
+
+        list.append("deep")
+
+        expect(list.to_string).to eq("doop deep")
+      end
+    end
+
+    context "#count_node" do
+      it "counts individual nodes" do
+        node = Node.new("doop")
+        
+        expect(list.count_node(node, 1)).to eq(1)
       end
     end
   end
