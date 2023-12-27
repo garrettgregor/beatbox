5477b016fe5d0bf872b01a121ad9bff6635825b1
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-27 08:33:26 -0500

    feat: #tail?

--- lib/node.rb
+++ lib/node.rb
@@ -1,8 +1,12 @@
 class Node
-  attr_reader :data, :next_node
+  attr_accessor :data, :next_node
 
   def initialize(string)
     @data       = string
     @next_node  = nil
+  end
+
+  def tail?
+    @next_node.nil?
   end
 end
